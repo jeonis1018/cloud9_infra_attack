615d1a0019f0063779f20b795d81bab53c58cf31
Yeoo-jin <yjgim0521@naver.com>
2026-08-17 23:46:30 +0900

    feat: s3-endpoint 모듈 구현
    - S3 버킷 + VPC Gateway Endpoint 생성
    - allowed_vpc_endpoint_only 스위치로 before/after 정책 분기
    - 공격 시연용 더미데이터 추가

---
+++ modules/s3-endpoint/outputs.tf
@@ -0,0 +1,14 @@
+output "bucket_id" {
+  value       = aws_s3_bucket.target.id
+  description = "attack 스크립트의 유출 타겟 버킷명"
+}
+
+output "bucket_arn" {
+  value       = aws_s3_bucket.target.arn
+  description = "iam 모듈에서 참조"
+}
+
+output "vpc_endpoint_id" {
+  value       = aws_vpc_endpoint.s3.id
+  description = "참고/로깅용"
+}
